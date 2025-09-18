5347764a59976c1f5cac234d123c205079dd33e9
Murad <quliyev.murad@yahoo.com>
2025-09-18 20:50:40 +0400

    InitializationHolderSingleton

--- singleton/Main.java
+++ singleton/Main.java
@@ -1,6 +1,8 @@
 package singleton;
 
+import singleton.lazy.InitializationHolderSingleton;
 import singleton.lazy.LazyNotThreadSafeSingleton;
+import singleton.lazy.LazyThreadSafeButCleanOneSingleton;
 
 public class Main {
 
@@ -10,12 +12,21 @@ public class Main {
         var eager = EagerSingleton.getInstance();
         var eager2 = EagerSingleton.getInstance();
         System.out.println("Confirmation eager: " + (eager == eager2));
-        eager.getConfiguration();
 
         // Lazy Not Thread Safe
         var lazyNotThreadSafe = LazyNotThreadSafeSingleton.getInstance();
         var lazyNotThreadSafe2 = LazyNotThreadSafeSingleton.getInstance();
         System.out.println("Confirmation lazy not thread safe: " + (lazyNotThreadSafe == lazyNotThreadSafe2));
+
+        // Lazy Thread Safe
+        var lazyThreadSafe = LazyThreadSafeButCleanOneSingleton.getInstance();
+        var lazyThreadSafe2 = LazyThreadSafeButCleanOneSingleton.getInstance();
+        System.out.println(lazyThreadSafe == lazyThreadSafe2);
+
+        // Initialization Holder
+        var initializationHolder = InitializationHolderSingleton.getInstance();
+        var initializationHolder2 = InitializationHolderSingleton.getInstance();
+        System.out.println(initializationHolder == initializationHolder2);
 
 
     }
--- singleton/lazy/InitializationHolderSingleton.java
+++ singleton/lazy/InitializationHolderSingleton.java
@@ -1,4 +1,20 @@
 package singleton.lazy;
 
 public class InitializationHolderSingleton {
+
+    private InitializationHolderSingleton() {
+
+    }
+
+    private static class InitializeHolder {
+        private final static InitializationHolderSingleton INSTANCE = new InitializationHolderSingleton();
+    }
+
+    public static InitializationHolderSingleton getInstance() {
+        return InitializeHolder.INSTANCE;
+    }
+
+    public void getConfiguration() {
+        System.out.println("Loading configuration...");
+    }
 }
